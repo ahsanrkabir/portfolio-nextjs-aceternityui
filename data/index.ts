71df71beac42b8805c74e208e918c6ff78df786f
Ahsan Kabir <ahsanrifat001@gmail.com>
2024-10-06 23:37:58 +0600

    add dummy testimonials

--- data/index.ts
+++ data/index.ts
@@ -111,33 +111,33 @@ export const projects = [
 export const testimonials = [
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+      "Collaborating with Kabir was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Kabir's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Kabir is the ideal partner.",
+    name: "John Doe",
+    title: "Director of JohnDoe Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+      "Collaborating with Kabir was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Kabir's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Kabir is the ideal partner.",
+    name: "John Doe",
+    title: "Director of JohnDoe Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+      "Collaborating with Kabir was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Kabir's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Kabir is the ideal partner.",
+    name: "John Doe",
+    title: "Director of JohnDoe Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+      "Collaborating with Kabir was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Kabir's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Kabir is the ideal partner.",
+    name: "John Doe",
+    title: "Director of JohnDoe Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-    name: "Michael Johnson",
-    title: "Director of AlphaStream Technologies",
+      "Collaborating with Kabir was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Kabir's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Kabir is the ideal partner.",
+    name: "John Doe",
+    title: "Director of JohnDoe Technologies",
   },
 ];
 
